1592c2522276e3d4cddb328bcb7591292b65879c
Nathan Ikola <nathan.ikola@gmail.com>
2021-03-30 16:16:48 -0700

    Implement basic message reading
    Implement message reading with fragmentation support (likely to be
    removed later as the current method ensures there will never be
    fragmentation since all reads are of a specific size)

--- ConnectionManagerCS/ConnectionManager.cs
+++ ConnectionManagerCS/ConnectionManager.cs
@@ -11,6 +11,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Net;
 using ConnectionManager.Protocols;
 
 namespace ConnectionManager
@@ -19,19 +20,71 @@ namespace ConnectionManager
     {
         public ConnectionManager(IConnectionProtocol protocol)
         {
+            Fragment = new byte[0];
             if (protocol == null) throw new ArgumentNullException();
             Protocol = protocol;
         }
 
         public Message Read()
         {
-            return null;
+            byte[] header = new byte[5];
+            if(Fragment.Length < header.Length)
+            {
+                byte[] secondFragment = Protocol.ReadMessage(header.Length - Fragment.Length);
+                for(int _byte = 0; _byte < header.Length; ++_byte)
+                {
+                    if (_byte < Fragment.Length) header[_byte] = Fragment[_byte];
+                    else header[_byte] = secondFragment[_byte - Fragment.Length];
+                }
+                Fragment = new byte[0];
+            }
+            else
+            {
+                for (int _byte = 0; _byte < header.Length; ++_byte)
+                    header[_byte] = Fragment[_byte];
+
+                byte[] resizedFragment = new byte[Fragment.Length - header.Length];
+                for (int _byte = header.Length; _byte < Fragment.Length; ++_byte)
+                    resizedFragment[_byte - header.Length] = Fragment[_byte];
+                Fragment = resizedFragment;
+            }
+
+            int payloadSize = BitConverter.ToInt32(header, 1);
+            payloadSize = IPAddress.NetworkToHostOrder(payloadSize) & 0xFFFFFF;
+            byte[] payload = new byte[payloadSize];
+
+            if (Fragment.Length < payloadSize)
+            {
+                byte[] secondFragment = Protocol.ReadMessage(payloadSize - Fragment.Length);
+                for(int _byte = 0; _byte < payloadSize; ++_byte)
+                {
+                    if (_byte < Fragment.Length) payload[_byte] = Fragment[_byte];
+                    else payload[_byte] = secondFragment[_byte - Fragment.Length];
+                }
+                Fragment = new byte[0];
+            }
+            else
+            {
+                for (int _byte = 0; _byte < payloadSize; ++_byte)
+                    payload[_byte] = Fragment[_byte];
+
+                byte[] resizedFragment = new byte[Fragment.Length - payloadSize];
+                for (int _byte = payloadSize; _byte < Fragment.Length; ++_byte)
+                    resizedFragment[_byte - payloadSize] = Fragment[_byte];
+                Fragment = resizedFragment;
+            }
+
+            byte[] messageBytes = new byte[header.Length + payloadSize];
+            header.CopyTo(messageBytes, 0);
+            payload.CopyTo(messageBytes, header.Length);
+
+            return Message.ParseNetworkBytes(messageBytes);
         }
 
         //******************************
         // Attributes
         //******************************
         private IConnectionProtocol Protocol { get; set; }
-        private List<byte> Fragment { get; set; }
+        private byte[] Fragment { get; set; }
     }
 }
