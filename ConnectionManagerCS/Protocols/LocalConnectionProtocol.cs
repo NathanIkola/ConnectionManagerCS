9510d52b1c66e6734b22b7abe184d58bdd53d15b
Nathan Ikola <nathan.ikola@gmail.com>
2021-04-05 22:35:55 -0700

    Add LocalConnectionProtocol object
    Add a connection protocol that simply reflects its input back into its
    output queue. Intended use is to derive a Connection object from it and
    add it to the Client pool of the IListener object handling external
    communications so that it is just like all of the other separate
    connections.

---
+++ ConnectionManagerCS/Protocols/LocalConnectionProtocol.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConnectionManagerCS.Listeners;
+
+namespace ConnectionManagerCS.Protocols
+{
+    public class LocalConnectionProtocol : IConnectionProtocol
+    {
+        public LocalConnectionProtocol()
+        {
+            Messages = new Queue<byte[]>();
+        }
+
+        public void WriteBytes(byte[] messageBytes)
+        {
+            Messages.Enqueue(messageBytes);
+        }
+
+        public byte[] ReadBytes(int messageLength)
+        {
+            return Messages.Dequeue();
+        }
+
+        public bool IsAlive()
+        {
+            return true;
+        }
+
+
+        private Queue<byte[]> Messages { get; set; }
+    }
+}
